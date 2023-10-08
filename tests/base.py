6e58a27c4dbdf53fdd78ba34315af62fee7696cd
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-10-08 20:47:30 +0200

    upgraded deep compare
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/base.py
+++ tests/base.py
@@ -75,12 +75,16 @@ class DeepCompareMixin(object):
             self.maxDiff = _omd
 
     def __deepDict(self, o: Any) -> Any:
-        if isinstance(o, (list, tuple)):
+        if isinstance(o, tuple):
             return tuple(self.__deepDict(i) for i in o)
+        if isinstance(o, list):
+            return list(self.__deepDict(i) for i in o)
         if isinstance(o, dict):
-            return {k: self.__deepDict(v) for k, v in o}
-        if isinstance(o, set):
-            return tuple(sorted((self.__deepDict(i) for i in o), key=repr))
+            return {k: self.__deepDict(v) for k, v in o.items()}
+        if isinstance(o, (set, SortedSet)):
+            # this method returns dict. `dict` is not hashable, so use `list` instead.
+            return list(self.__deepDict(i) for i in o) + ['%conv:%set']
         if hasattr(o, '__dict__'):
-            return {k: self.__deepDict(v) for k, v in vars(o).items() if not (k.startswith('__') and k.endswith('__'))}
+            return {a: self.__deepDict(v) for a, v in o.__dict__.items() if '__' not in a
+                    } | {'%conv': str(type(o))}
         return o
