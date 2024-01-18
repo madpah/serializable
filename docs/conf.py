ec56c4618f1a3acf73a24fedc3011cd065744e20
semantic-release <semantic-release>
2024-01-18 18:25:36 +0000

    chore(release): 1.0.0-rc.1
    Automatically generated by python-semantic-release
    
    Signed-off-by: semantic-release <semantic-release>

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Paul Horton'
 
 # The full version, including alpha/beta/rc tags
 # !! version is managed by semantic_release
-release = "1.0.0-rc.0"
+release = "1.0.0-rc.1"
 
 # -- General configuration ---------------------------------------------------
 
