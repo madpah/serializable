06776baef2cc4b893550320c474128317f6276c1
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-02-14 00:21:33 +0100

    fix: serialization of `datetime` without timezone with local time offset (#76)
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- serializable/helpers.py
+++ serializable/helpers.py
@@ -176,10 +176,21 @@ class XsdDate(BaseHelper):
 
 class XsdDateTime(BaseHelper):
 
+    @staticmethod
+    def __fix_tz(dt: datetime) -> datetime:
+        """
+        Fix for Python's violation of ISO8601: :py:meth:`datetime.isoformat()` might omit the time offset when in doubt,
+        but the ISO-8601 assumes local time zone.
+        Anyway, the time offset is mandatory for this purpose.
+        """
+        return dt.astimezone() \
+            if dt.tzinfo is None \
+            else dt
+
     @classmethod
     def serialize(cls, o: Any) -> str:
         if isinstance(o, datetime):
-            return o.isoformat()
+            return cls.__fix_tz(o).isoformat()
 
         raise ValueError(f'Attempt to serialize a non-date: {o.__class__}')
 
